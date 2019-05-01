c6b65a3cb8fe63f05939b8a64246f11314761daf
koooge <koooooge@gmail.com>
2019-05-01 16:12:40 +0000

    refactor: Use if-else
    Signed-off-by: koooge <koooooge@gmail.com>

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -11,12 +11,12 @@ function Item(name, sell_in, quality) {
     this.sell_in--;
   }
 
-  this.increaseQuality = function() {
-    if (this.quality < 50) this.quality++;
+  this.increaseQuality = function(val = 1) {
+    if (this.quality < 50) this.quality += val;
   }
 
-  this.decreaseQuality = function() {
-    if (this.quality > 0) this.quality--;
+  this.decreaseQuality = function(val = 1) {
+    if (this.quality > 0) this.quality -= val;
   }
 }
 
@@ -31,17 +31,22 @@ function update_quality() {
     } else if (items[i].name === sulfuras) {
     } else if (items[i].name === backstagePass) {
       items[i].getOld();
-      if (items[i].sell_in >= 0) {
-        items[i].increaseQuality();
-        if (items[i].sell_in < 10) items[i].increaseQuality();
-        if (items[i].sell_in < 5) items[i].increaseQuality();
+      if (items[i].sell_in >= 10) {
+        items[i].increaseQuality(1);
+      } else if (items[i].sell_in < 10 && items[i].sell_in >= 5) {
+        items[i].increaseQuality(2);
+      } else if (items[i].sell_in < 5 && items[i].sell_in >= 0) {
+        items[i].increaseQuality(3);
       } else {
         items[i].quality = 0;
       }
     } else {
       items[i].getOld();
-      items[i].decreaseQuality();
-      if (items[i].sell_in < 0) items[i].decreaseQuality();
+      if (items[i].sell_in >= 0) {
+        items[i].decreaseQuality(1);
+      } else {
+        items[i].decreaseQuality(2);
+      }
     }
   }
 }
